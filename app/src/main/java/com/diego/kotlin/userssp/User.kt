8250695a04f9469843c6ee815b675712f025b1d8
balay <dlolhd@gmail.com>
2022-01-03 11:09:48 +0100

    Commit de diseño de RecyclerView y Data Class

---
+++ app/src/main/java/com/diego/kotlin/userssp/User.kt
@@ -0,0 +1,8 @@
+package com.diego.kotlin.userssp
+
+/**
+ * clase especializada en modelo de datos para kotlin 'data class'
+ */
+data class User (val id: Long, var name: String, var lastName: String, var url: String) {
+
+}

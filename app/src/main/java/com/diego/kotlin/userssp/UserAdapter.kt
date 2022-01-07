d60f798b1d3a505d1278f2349cd848d85bd4ccd3
balay <dlolhd@gmail.com>
2022-01-07 11:00:13 +0100

    Commit de Vista alternativa.

--- app/src/main/java/com/diego/kotlin/userssp/UserAdapter.kt
+++ app/src/main/java/com/diego/kotlin/userssp/UserAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.diego.kotlin.userssp.databinding.ItemUserAltBinding
 import com.diego.kotlin.userssp.databinding.ItemUserBinding
 
 class UserAdapter(private val users: List<User>, private val listener: OnClickListener) :
@@ -20,7 +21,7 @@ class UserAdapter(private val users: List<User>, private val listener: OnClickLi
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         context = parent.context
 
-        val view = LayoutInflater.from(context).inflate(R.layout.item_user, parent, false)
+        val view = LayoutInflater.from(context).inflate(R.layout.item_user_alt, parent, false)
 
         return ViewHolder(view)
     }
@@ -51,7 +52,7 @@ class UserAdapter(private val users: List<User>, private val listener: OnClickLi
     override fun getItemCount(): Int = users.size
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
-        val binding = ItemUserBinding.bind(view)
+        val binding = ItemUserAltBinding.bind(view)
 
         fun setListener(user: User, position: Int){
             binding.root.setOnClickListener { listener.onClick(user, position) }
